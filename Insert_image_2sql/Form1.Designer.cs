319f43f7aa74f66b52f9b99c40e253214a6d552c
devkorn555 <82007106+devkorn555@users.noreply.github.com>
2021-05-17 13:33:16 +0700

    Add files via upload

---
+++ Insert_image_2sql/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Insert_image_2sql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formSkin1 = new FlatUI.FormSkin();
+            this.flatMini1 = new FlatUI.FlatMini();
+            this.flatClose1 = new FlatUI.FlatClose();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbpath = new FlatUI.FlatTextBox();
+            this.flatButton2 = new FlatUI.FlatButton();
+            this.flatButton1 = new FlatUI.FlatButton();
+            this.formSkin1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // formSkin1
+            // 
+            this.formSkin1.BackColor = System.Drawing.Color.White;
+            this.formSkin1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.formSkin1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(58)))), ((int)(((byte)(60)))));
+            this.formSkin1.Controls.Add(this.flatMini1);
+            this.formSkin1.Controls.Add(this.flatClose1);
+            this.formSkin1.Controls.Add(this.pictureBox1);
+            this.formSkin1.Controls.Add(this.tbpath);
+            this.formSkin1.Controls.Add(this.flatButton2);
+            this.formSkin1.Controls.Add(this.flatButton1);
+            this.formSkin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formSkin1.FlatColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.formSkin1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.formSkin1.HeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.formSkin1.HeaderMaximize = false;
+            this.formSkin1.Location = new System.Drawing.Point(0, 0);
+            this.formSkin1.Name = "formSkin1";
+            this.formSkin1.Size = new System.Drawing.Size(879, 791);
+            this.formSkin1.TabIndex = 0;
+            this.formSkin1.Text = "C#  INSERT PICTURE TO SQL SERVER!";
+            // 
+            // flatMini1
+            // 
+            this.flatMini1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flatMini1.BackColor = System.Drawing.Color.White;
+            this.flatMini1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.flatMini1.Font = new System.Drawing.Font("Marlett", 12F);
+            this.flatMini1.Location = new System.Drawing.Point(816, 12);
+            this.flatMini1.Name = "flatMini1";
+            this.flatMini1.Size = new System.Drawing.Size(18, 18);
+            this.flatMini1.TabIndex = 4;
+            this.flatMini1.Text = "flatMini1";
+            this.flatMini1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            // 
+            // flatClose1
+            // 
+            this.flatClose1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flatClose1.BackColor = System.Drawing.Color.White;
+            this.flatClose1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.flatClose1.Font = new System.Drawing.Font("Marlett", 10F);
+            this.flatClose1.Location = new System.Drawing.Point(849, 12);
+            this.flatClose1.Name = "flatClose1";
+            this.flatClose1.Size = new System.Drawing.Size(18, 18);
+            this.flatClose1.TabIndex = 3;
+            this.flatClose1.Text = "flatClose1";
+            this.flatClose1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Insert_image_2sql.Properties.Resources.question_mark_2_256;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(834, 595);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbpath
+            // 
+            this.tbpath.BackColor = System.Drawing.Color.Transparent;
+            this.tbpath.FocusOnHover = false;
+            this.tbpath.Location = new System.Drawing.Point(134, 68);
+            this.tbpath.MaxLength = 32767;
+            this.tbpath.Multiline = false;
+            this.tbpath.Name = "tbpath";
+            this.tbpath.ReadOnly = false;
+            this.tbpath.Size = new System.Drawing.Size(492, 29);
+            this.tbpath.TabIndex = 1;
+            this.tbpath.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.tbpath.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.tbpath.UseSystemPasswordChar = false;
+            // 
+            // flatButton2
+            // 
+            this.flatButton2.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton2.Location = new System.Drawing.Point(386, 721);
+            this.flatButton2.Name = "flatButton2";
+            this.flatButton2.Rounded = false;
+            this.flatButton2.Size = new System.Drawing.Size(106, 32);
+            this.flatButton2.TabIndex = 0;
+            this.flatButton2.Text = "Save";
+            this.flatButton2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton2.Click += new System.EventHandler(this.flatButton2_Click);
+            // 
+            // flatButton1
+            // 
+            this.flatButton1.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton1.Location = new System.Drawing.Point(22, 65);
+            this.flatButton1.Name = "flatButton1";
+            this.flatButton1.Rounded = false;
+            this.flatButton1.Size = new System.Drawing.Size(106, 32);
+            this.flatButton1.TabIndex = 0;
+            this.flatButton1.Text = "Select File";
+            this.flatButton1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton1.Click += new System.EventHandler(this.flatButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 791);
+            this.Controls.Add(this.formSkin1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.formSkin1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlatUI.FormSkin formSkin1;
+        private FlatUI.FlatButton flatButton1;
+        private FlatUI.FlatTextBox tbpath;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private FlatUI.FlatButton flatButton2;
+        private FlatUI.FlatMini flatMini1;
+        private FlatUI.FlatClose flatClose1;
+    }
+}
+
---
+++ Insert_image_2sql/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace Insert_image_2sql
+{
+    public partial class Form1 : Form
+    {
+        string strcon = @"Data Source=DESKTOP-MPTIJRG\SQLEXPRESS;Initial Catalog=r_control;User ID=sa;PWD=12345";
+
+        byte[] img;
+        FileStream fs;
+        BinaryReader br;
+        string path = null;
+        string imgname = null;
+
+        SqlConnection con;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flatButton1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog di = new OpenFileDialog();
+            di.Filter = "Image File|*.jpg";
+            DialogResult dr = di.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                path = di.FileName;
+                tbpath.Text = path;
+                pictureBox1.Image = Image.FromFile(path);
+                pictureBox1.Refresh();
+
+                fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                br = new BinaryReader(fs);
+                img = br.ReadBytes(Convert.ToInt32(fs.Length));
+            }
+            else
+            {
+                path = null;
+                tbpath.Text = null;
+                pictureBox1.Image = Insert_image_2sql.Properties.Resources.question_mark_2_256;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void flatButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Connectdb();
+                string txt = "INSERT INTO Testimg(img) VALUES (@img)";
+                SqlCommand cmd = new SqlCommand(txt, con);
+
+                cmd.Parameters.Add(new SqlParameter("@img", img));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Suscess!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void Connectdb()
+        {
+            con = new SqlConnection(strcon);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Insert_image_2sql.Properties.Resources.question_mark_2_256;
+        }
+    }
+}
